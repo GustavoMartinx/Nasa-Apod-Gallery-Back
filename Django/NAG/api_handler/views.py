0749f77cbf43594ebaeaa1aeef7498dff73526bc
Gustavo Martins <gtavomartins893@gmail.com>
2024-01-27 12:14:50 -0300

    feat: done view funct to fetch api test + front connected

--- Django/NAG/api_handler/views.py
+++ Django/NAG/api_handler/views.py
@@ -1,32 +1,29 @@
 # Views is a request handler.
 
-from django.shortcuts import render
 from rest_framework import status
 from django.http import JsonResponse
 import requests
 
 def get_images(request):
     
-    # if (request.method == 'GET'):
-    #     num_images = int(request.GET['num_images'])
-    #     if (0 < num_images <= 9):
+    if (request.method == 'GET'):
+        
+        # getting the number of images to be fetched
+        num_images = int(request.GET['num_images'])
+        if (0 < num_images <= 9):
 
-    #         url = 'https://source.unsplash.com/random'
-    #         response = []
+            url = 'https://source.unsplash.com/random'
+            response = []
 
-    #         for i in range(num_images):
-    #             api_response = requests.get(url)
+            # making the num_images as the number of requests
+            for i in range(num_images):
+                api_response = requests.get(url)
 
-    #             # Check if the request was successful (status code 200)
-    #             if api_response.status_code == 200:
-    #                 response.append(api_response.content)
-    #             else:
-    #                 return JsonResponse({'error': f'Failed to fetch image {i}'}, status=status.HTTP_400_BAD_REQUEST)
+                if api_response.status_code == 200:
+                    response.append(api_response.request.url)
+                else:
+                    return JsonResponse({'error': f'Failed to fetch image {i}'}, status=status.HTTP_400_BAD_REQUEST)
 
-    #         # send it back to frontend
-    #         return JsonResponse({'images': response}, status=status.HTTP_200_OK)
+            return JsonResponse({'images_urls': response}, status=status.HTTP_200_OK)
 
-    # return JsonResponse({'error': 'Invalid request'}, status=status.HTTP_400_BAD_REQUEST)
-
-    # just to test
-    return JsonResponse({'images': 'https://source.unsplash.com/random'}, status=status.HTTP_200_OK)
+    return JsonResponse({'error': 'Invalid request'}, status=status.HTTP_400_BAD_REQUEST)
