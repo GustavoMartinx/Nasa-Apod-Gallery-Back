c5fe7226aeb59505d821e47df69c7af12ab3cb8f
Gustavo Martins <gtavomartins893@gmail.com>
2024-02-16 15:50:30 -0300

    feat: view func to create new saved collection

--- Django/NAG/saved_collections/models.py
+++ Django/NAG/saved_collections/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class SavedCollections(models.Model):
 
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    # date_list = models.JSONField()
+    name = models.CharField(max_length=255)
+    dates = models.JSONField(default=list)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- Django/NAG/saved_collections/views.py
+++ Django/NAG/saved_collections/views.py
@@ -1,35 +1,26 @@
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-from allauth.socialaccount.models import SocialAccount
-from .models import SavedCollections
+from rest_framework.decorators import api_view
+
 from django.http import JsonResponse
 from rest_framework import status
-import json
+
+from .models import SavedCollections
 
 @csrf_exempt
-# @login_required
+@login_required
+@api_view(['POST'])
 def create_new_collection(request):
 
-    if request.method == 'POST':
-        # Decodifica o corpo da requisição para um dicionário Python
-        request_data = json.loads(request.body.decode('utf-8'))
-
-        collectionName = request_data.get('collection_name')
-        # dateList = request_data.get('date_list')
-        username = request_data.get('user')
-        
-        # print("username", username)
-        # print("collectionName", collectionName)
-        
-        try:
-            SavedCollections.objects.create(
-                name = collectionName,
-                # date_list = dateList,
-                user = username,
-            )
-        except:
-            return JsonResponse({'error': 'Something went wrong while creating the collection'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        return JsonResponse({'message': f'Collection {collectionName} created successfully!'}, status=status.HTTP_200_OK)
-    else:
-        return JsonResponse({'error': 'Invalid request'}, status=status.HTTP_400_BAD_REQUEST)
+    request_data = request.data
+    collectionName = request_data.get('collection_name')
+    
+    try:
+        SavedCollections.objects.create(
+            name = collectionName,
+            user = request.user,
+        )
+    except:
+        return JsonResponse({'error': 'Something went wrong while creating the collection'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    return JsonResponse({'message': f'Collection {collectionName} created successfully!'}, status=status.HTTP_201_CREATED)
