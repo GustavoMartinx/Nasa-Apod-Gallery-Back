8d2794b9c3de3910e40a236978fb4e1c30dde9dd
Gustavo Martins <gtavomartins893@gmail.com>
2024-02-20 15:32:20 -0300

    feat: created view+url to get every user's saved collections

--- Django/NAG/saved_collections/urls.py
+++ Django/NAG/saved_collections/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# saved-collections/
 urlpatterns = [
     path('create/', views.create_new_collection),
+    path('list/', views.list_saved_collections),
 ]
--- Django/NAG/saved_collections/views.py
+++ Django/NAG/saved_collections/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 
 from .models import SavedCollections
 
-@csrf_exempt
+# @csrf_exempt
 @login_required
 @api_view(['POST'])
 def create_new_collection(request):
@@ -16,11 +16,28 @@ def create_new_collection(request):
     collectionName = request_data.get('collection_name')
     
     try:
+        collection = SavedCollections.objects.get(name=collectionName, user=request.user)
+        return JsonResponse({'error': f'Collection {collectionName} already exists. Please choose a different name.'}, status=status.HTTP_400_BAD_REQUEST)
+    except SavedCollections.DoesNotExist:
+        pass
+    
+    try:
         SavedCollections.objects.create(
             name = collectionName,
             user = request.user,
         )
     except:
-        return JsonResponse({'error': 'Something went wrong while creating the collection'}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({'error': 'Something went wrong while creating the collection.'}, status=status.HTTP_400_BAD_REQUEST)
     
     return JsonResponse({'message': f'Collection {collectionName} created successfully!'}, status=status.HTTP_201_CREATED)
+
+
+@login_required
+@api_view(['GET'])
+def list_saved_collections(request):
+    try:
+        collections = SavedCollections.objects.filter(user=request.user)
+    except SavedCollections.DoesNotExist:
+        return JsonResponse({'error': 'No collections found.'}, status=status.HTTP_404_NOT_FOUND)
+    
+    return JsonResponse({'collections': [collection.name for collection in collections]}, status=status.HTTP_200_OK)
